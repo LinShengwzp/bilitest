dd25dc0229caae4c781693693f219d5f2cbf0bd1
生 林 <wzp129168@outlook.com>
2020-09-14 22:56:58 +0800

    至少能用滚动条了……

--- src/renderer/views/pegasus/promo.vue
+++ src/renderer/views/pegasus/promo.vue
@@ -1,24 +1,18 @@
 <template>
 
-    <div>
+  <div style="height: 100%">
+    <el-scrollbar style="height: 100%">
       <el-row v-for="item in indexNum" :key="item">
-        <el-col>sss{{item}}</el-col>
+        <el-col>sss{{ item }}</el-col>
       </el-row>
-      <!--    <waterfall-->
-      <!--      :col="col"-->
-      <!--      :width="itemWidth"-->
-      <!--      :gutterWidth="gutterWidth"-->
-      <!--      :data="indexData"-->
-      <!--      @loadmore="loadmore"-->
-      <!--      @scroll="scroll"-->
-      <!--    >-->
-      <!--    </waterfall>-->
-    </div>
+    </el-scrollbar>
+  </div>
 
 </template>
 
 <script>
 import ScrollBar from '@/components/ScrollBar'
+
 export default {
   name: 'promo', // 推荐
   data() {
@@ -37,10 +31,18 @@ export default {
     },
     gutterWidth() {
       return 9 * 0.5 * (document.documentElement.clientWidth / 375) // rem布局 计算x轴方向margin(y轴方向的margin自定义在css中即可)
+    },
+    currentHeight() {
+      return (document.documentElement.clientHeight - 150) + 'px'
     }
   },
   mounted() {
     console.log('你进来了吗')
+  },
+  watch: {
+    currentHeightWatch() {
+      console.log(document.documentElement.clientHeight)
+    }
   },
   methods: {
     scroll(scrollData) {
